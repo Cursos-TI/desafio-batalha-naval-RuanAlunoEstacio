f1a8deead773924a7a0a7ce1fef6b017d3352fc2
RuanAlunoEstacio <psoares3568@gmail.com>
2025-06-17 21:57:13 +0000

    comentário sobre linha

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -161,7 +161,7 @@ int main() {
     }
     for(int i = 0; i< 10; i++){
         if(i<9){
-            printf("\n %d ",i+1);
+            printf("\n %d ",i+1); //Imprime a linha do tabuleiro
         } else {
             printf("\n%d ",i+1);
         }
